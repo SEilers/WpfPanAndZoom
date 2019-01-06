21fd306381523ce8106cb62bea06f5276cc7973c
SEilers <soenke.eilers@gmail.com>
2019-01-06 14:09:44 +0100

    zooming on point

--- WpfPanAndZoom/MainWindow.xaml.cs
+++ WpfPanAndZoom/MainWindow.xaml.cs
@@ -20,28 +20,13 @@ namespace WpfPanAndZoom
     /// </summary>
     public partial class MainWindow : Window
     {
-        #region Variables
+        // https://stackoverflow.com/questions/35165349/how-to-drag-rendertransform-with-mouse-in-wpf
 
-        float _zoom = 1;
+        #region Variables
+        private readonly MatrixTransform transform = new MatrixTransform();
+        private Point pressedMouse;
 
         bool dragging;
-
-        bool panning = false;
-
-        double startPanningX;
-        double startPanningY;
-
-        double endPanningX;
-        double endPanningY;
-
-        double dx;
-        double dy;
-
-        TransformGroup transformGroup = new TransformGroup();
-
-        Transform translation = new System.Windows.Media.TranslateTransform(0, 0);
-        Transform scale = new System.Windows.Media.ScaleTransform(1, 1);
-
         #endregion
 
 
@@ -49,75 +34,61 @@ namespace WpfPanAndZoom
         {
             InitializeComponent();
 
-            mwGrid.MouseWheel += MwGrid_MouseWheel;
-            mwGrid.MouseDown += MwGrid_MouseDown;
-            mwGrid.MouseMove += MwGrid_MouseMove;
-            mwGrid.MouseUp += MwGrid_MouseUp;
-
-            transformGroup.Children.Add(scale);
-            transformGroup.Children.Add(translation);
+            canvas.MouseWheel += MwGrid_MouseWheel;
+            canvas.MouseDown += MwGrid_MouseDown;
+            canvas.MouseMove += MwGrid_MouseMove;
+            canvas.MouseUp += MwGrid_MouseUp;
 
             CustomControls.Widget w1 = new CustomControls.Widget();
             w1.Width = 200;
             w1.Height = 150;
-            mwGrid.Children.Add(w1);
-
-            w1.Border.BorderThickness = new Thickness(3);
+            canvas.Children.Add(w1);
             w1.Header.Text = "Widget 1";
-           
-            transformGroup.Children[1] = new TranslateTransform(10, 10);
-            w1.RenderTransform = transformGroup;
+            Canvas.SetTop(w1, 100);
+            Canvas.SetLeft(w1, 100);
 
-            //CustomControls.Widget w2 = new CustomControls.Widget();
-            //w2.Width = 200;
-            //w2.Height = 150;
-            //mwGrid.Children.Add(w2);
+            CustomControls.Widget w2 = new CustomControls.Widget();
+            w2.Width = 200;
+            w2.Height = 150;
+            canvas.Children.Add(w2);
+            w2.Header.Text = "Widget 2";
+            w2.HeaderRectangle.Fill = Brushes.Blue;
+            Canvas.SetTop(w2, 400);
+            Canvas.SetLeft(w2, 400);
 
-            //transformGroup.Children[1] = new TranslateTransform(200, 200);
-            //w2.RenderTransform = transformGroup;
-
-        
-          
-
-
-
-
-
-
-
-
+            CustomControls.Widget w3 = new CustomControls.Widget();
+            w3.Width = 200;
+            w3.Height = 150;
+            canvas.Children.Add(w3);
+            w3.Header.Text = "Widget 3";
+            w3.HeaderRectangle.Fill = Brushes.Red;
+            Canvas.SetTop(w3, 400);
+            Canvas.SetLeft(w3, 800);
         }
 
         private void MwGrid_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            panning = false;
-            endPanningX = dx;
-            endPanningY = dy;
+            dragging = false;
         }
 
         private void MwGrid_MouseMove(object sender, MouseEventArgs e)
         {
-            if (panning && e.RightButton == MouseButtonState.Pressed)
+            if (e.RightButton == MouseButtonState.Pressed)
             {
-                double currentX = Mouse.GetPosition(this).X;
-                double currentY = Mouse.GetPosition(this).Y;
-                dx = currentX - startPanningX;
-                dy = currentY - startPanningY;
-                transformGroup.Children[1] = new TranslateTransform(endPanningX + dx, endPanningY + dy);
-                mwGrid.RenderTransform = transformGroup;
+                Point mouse = transform.Inverse.Transform(e.GetPosition(this));
+                Vector delta = Point.Subtract(mouse, pressedMouse); // delta from old mouse to current mouse
+                var translate = new TranslateTransform(delta.X, delta.Y);
+                transform.Matrix = translate.Value * transform.Matrix;
+
+                ((UIElement)Content).RenderTransform = transform;
+                e.Handled = true;
+                //canvas.RenderTransform = transform;
             }
 
             if (dragging && e.LeftButton == MouseButtonState.Pressed)
             {
-                double x = Mouse.GetPosition(mwGrid).X;
-                double y = Mouse.GetPosition(mwGrid).Y;
-
-                //if (selectedControl != null)
-                //{
-                //    //selectedControl.RenderTransform = new TranslateTransform(x, y);
-                //    Canvas.SetLeft(selectedControl, x);
-                //    Canvas.SetTop(selectedControl, y);
-                //}
+                double x = Mouse.GetPosition(canvas).X;
+                double y = Mouse.GetPosition(canvas).Y;
             }
         }
 
@@ -125,9 +96,7 @@ namespace WpfPanAndZoom
         {
             if (e.ChangedButton == MouseButton.Right)
             {
-                panning = true;
-                startPanningX = Mouse.GetPosition(this).X;
-                startPanningY = Mouse.GetPosition(this).Y;
+                pressedMouse = transform.Inverse.Transform(e.GetPosition(this));
             }
 
             if (e.ChangedButton == MouseButton.Left)
@@ -136,20 +105,22 @@ namespace WpfPanAndZoom
             }
         }
 
+      
+
         private void MwGrid_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            if (e.Delta > 0)
-            {
-                _zoom *= 1.1f;
-            }
-            else
-            {
-                _zoom *= 0.9f;
-            }
+            float scale = 1.1f;
+            if (e.Delta < 0) scale = 1f / scale;
+            Point mouse = e.GetPosition(this);
 
-            transformGroup.Children[0] = new System.Windows.Media.ScaleTransform(_zoom, _zoom);
+            Matrix matrix = transform.Matrix;
+            matrix.ScaleAt(scale, scale, mouse.X, mouse.Y);
+            transform.Matrix = matrix;
 
-            mwGrid.RenderTransform = transformGroup;
+            ((UIElement)Content).RenderTransform = transform;
+            e.Handled = true;
+
+            //canvas.RenderTransform = transform;
         }
     }
 }
